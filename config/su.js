ad9a8cb9a2a1c6f3bc67ab17f866e9f0d119b340
Su <hesu@newbanker.cn>
2018-11-16 11:00:42 +0800

    1103 财富查项目总结

--- config/su.js
+++ config/su.js
@@ -3,7 +3,8 @@ module.exports = {
     title: '苏鹤',
     children: [
       ['/docs/su/markdown', 'markdown语法入门'],
-      ['/docs/su/debounceAndThrottle', '函数防抖与函数节流']
+      ['/docs/su/debounce', '函数防抖与函数节流'],
+      ['/docs/su/review', '天眼查项目review']
     ]
   }
 }
