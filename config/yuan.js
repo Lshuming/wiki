9b3aafd3bd94757c91dd632bb22edf2f5c6c50aa
Su <hesu@newbanker.cn>
2018-11-16 11:06:10 +0800

    修改靖雅的目录结构

--- config/yuan.js
+++ config/yuan.js
@@ -2,7 +2,6 @@ module.exports = {
   sidebar: {
     title: '袁靖雅',
     children: [
-      ['/docs/yuan/guide', '袁靖雅'],
       ['/docs/yuan/dash', '财富查项目总结'],
       ['/docs/yuan/array', '数组方法介绍']
     ]
